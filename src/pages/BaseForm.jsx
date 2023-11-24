9546b92b9b1793f9822a1e0bf9041197c97ed2fd
Nisha Yadav <nishayadav1703@gmail.com>
2023-11-25 00:14:53 +0530

    openRetros api integration for particular retro id

---
+++ src/pages/BaseForm.jsx
@@ -0,0 +1,161 @@
+import { useEffect, useState } from "react";
+import BaseIcon from "@/components/BaseIcon";
+import { ICONS } from "@/helpers/constant";
+import boardService from "@/services/board.service";
+import { useNavigate } from "react-router-dom";
+
+export default function BaseForm(props) {
+  const { children } = props;
+  const [isOpen, setIsOpen] = useState(false);
+  const [boardModel, setBoardModel] = useState({});
+  const navigate = useNavigate();
+
+  const openModal = () => {
+    setIsOpen(true);
+  };
+
+  const closeModal = () => {
+    setIsOpen(false);
+  };
+
+  const handleChangeName = (event) => {
+    console.log("handleChangeName");
+    event.preventDefault();
+
+    const { name, value } = event.target;
+    console.log(value);
+
+    setBoardModel({
+      ...boardModel,
+      [name]: value,
+    });
+  };
+
+  const handleChangeDate = () => {
+    console.log("handleChangeDate called");
+  };
+
+  const handleChangeId = () => {
+    console.log("handleChangeId");
+  };
+
+  const getRetros = async () => {
+    try {
+      const boards = await boardService.getBoards();
+      // console.log(`boards`);
+      console.log(boards);
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
+  useEffect(() => {
+    getRetros();
+  });
+
+  const handleSave = async (e) => {
+    e.preventDefault();
+    try {
+      //   const newBoardId = await boardService.createBoard(boardModel);
+      //   console.log(`newBoardId`);
+      //   console.log(newBoardId);
+      //   navigate(`/${newBoardId}`);
+      closeModal();
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
+  const cancelEvent = (e) => {
+    console.log("event", e);
+  };
+
+  return (
+    <>
+      <div onClick={openModal}>{children}</div>
+      {isOpen && (
+        <form onSubmit={handleSave}>
+          <div
+            className="relative z-10"
+            aria-labelledby="modal-title"
+            role="dialog"
+            aria-modal="true"
+          >
+            <div className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity"></div>
+            <div className="fixed inset-0 z-10 w-screen overflow-y-auto">
+              <div className="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
+                <div className="relative transform overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg">
+                  <div className="bg-neutral-100 flex gap-8 flex-col justify-center px-4 pb-4  sm:p-6 sm:pb-4">
+                    <div className="flex gap-2 justify-center items-center">
+                      <BaseIcon
+                        iconName={ICONS.rocket}
+                        className=" flex w-5 h-5 text-blue-500"
+                      ></BaseIcon>{" "}
+                      <h1 className="text-xl">
+                        Open <b className=" text-gray-500">Retro</b>
+                      </h1>
+                    </div>
+                    <div className="flex gap-2 justify-center items-center">
+                      <label htmlFor="" className="font-semibold">
+                        {" "}
+                        Retro Name :
+                      </label>
+                      <input
+                        type="text"
+                        name="boardName"
+                        className="border-2 py-1 px-2 border-gray-300 rounded-sm"
+                        onChange={handleChangeName}
+                      />
+                    </div>
+
+                    <div className="flex gap-2 justify-center items-center">
+                      <label htmlFor="" className="font-semibold">
+                        {" "}
+                        Date :
+                      </label>
+                      <input
+                        type="text"
+                        name="boardName"
+                        className="border-2 py-1 px-2 border-gray-300 rounded-sm"
+                        onChange={handleChangeDate}
+                      />
+                    </div>
+
+                    <div className="flex gap-2 justify-center items-center">
+                      <label htmlFor="" className="font-semibold">
+                        {" "}
+                        Board Id :
+                      </label>
+                      <input
+                        type="text"
+                        name="boardName"
+                        className="border-2 py-1 px-2 border-gray-300 rounded-sm"
+                        onChange={handleChangeId}
+                      />
+                    </div>
+
+                    <div className="flex gap-2 justify-end">
+                      <button
+                        type="button"
+                        onClick={closeModal}
+                        className=" hover:bg-red-700 py-1 px-3 text-red-500 hover:text-white rounded-sm"
+                      >
+                        Cancel
+                      </button>
+                      <button
+                        type="submit"
+                        className="hover:text-white hover:bg-blue-500 py-1 px-3 text-blue-500 rounded-sm"
+                      >
+                        Submit
+                      </button>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </form>
+      )}
+    </>
+  );
+}
--- src/pages/OpenRetros.jsx
+++ src/pages/OpenRetros.jsx
@@ -3,24 +3,28 @@ import { useParams } from "react-router-dom";
 import BaseIcon from "../components/BaseIcon";
 import { ICONS } from "../helpers/constant";
 import BaseNavbar from "./BaseNavbar";
-import boardService from "@/services/board.service";
+// import boardService from "@/services/board.service";
+import BaseForm from "./BaseForm";
+import { db } from "@/firebase";
+import { collection, query, where, onSnapshot } from "firebase/firestore";
 
 export default function OpenRetros() {
   const [retros, setRetros] = useState([]);
   const params = useParams();
   console.log("params", params);
-  console.log("retros", retros);
 
-  const getRetrosDetail = async () => {
-    try {
-      const retros = await boardService.getRetros();
-      setRetros(retros);
-    } catch (e) {
-      console.log(e);
-    }
-  };
   useEffect(() => {
-    getRetrosDetail();
+    const boardRef = collection(db, "retros");
+    const q = query(boardRef, where("boardId", "==", "HYHe9hVonGIwRxSGFjLm"));
+
+    return onSnapshot(q, (snapShot) => {
+      let retroDetails = [];
+      snapShot.docs.forEach((doc) => {
+        retroDetails.push({ ...doc.data(), id: doc.id });
+      });
+      // console.log("retroDetails", retroDetails);
+      setRetros(retroDetails);
+    });
   }, []);
   const retroClick = (path) => {
     console.log("retroClick called", path);
@@ -32,18 +36,21 @@ export default function OpenRetros() {
 
       <div className="bg-[#F1F2F5] px-8 py-8">
         <div className=" flex py-4 gap-4">
-          <button
-            type="button"
-            className="border-2 border-neutral-600 hover:border-blue-500 hover:text-blue-500 border-dashed h-40 w-60 flex flex-col gap-1 justify-center items-center rounded-md"
-          >
-            <div className=" bg-[#C0C0D4] rounded-full px-2 py-2">
-              <BaseIcon
-                iconName={ICONS.Plus}
-                className=" flex h-6 w-6 text-white"
-              ></BaseIcon>
-            </div>
-            <h1 className="text-sm">Create boards</h1>
-          </button>
+          <BaseForm>
+            <button
+              type="button"
+              className="border-2 border-neutral-600 hover:border-blue-500 hover:text-blue-500 border-dashed h-40 w-60 flex flex-col gap-1 justify-center items-center rounded-md"
+            >
+              <div className=" bg-[#C0C0D4] rounded-full px-2 py-2">
+                <BaseIcon
+                  iconName={ICONS.Plus}
+                  className=" flex h-6 w-6 text-white"
+                ></BaseIcon>
+              </div>
+              <h1 className="text-sm">Create boards</h1>
+            </button>
+          </BaseForm>
+
           {retros.map((retroDetails, index) => {
             return (
               //   <h1 key={retroDetails.retroName + index}>
