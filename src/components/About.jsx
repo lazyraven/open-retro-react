cc48c7c358fdb4a8c9cbdb3eb0e9d07057eece94
Lakshya yadav <itslakshyayadav@gmail.com>
2024-02-24 00:52:55 +0530

    commit

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -7,7 +7,7 @@ export default function About() {
       <div className="container w-7/12 m-auto text-lg text-gray-600 font-medium flex flex-col gap-8 leading-8">
         <h3 className="">
           If you’ve got the fun, exciting, and innovative team - we have the
-          tool. EasyRetro is a fun retrospective (‘retro’ for short) tool you
+          tool. OpenRetro is a fun retrospective (‘retro’ for short) tool you
           can use for your regular sprint session to discuss what went well and
           what didn’t. You’ll be able to easily identify obstacles and discuss
           ideas for improvements that will allow you to move forward unhindered.
@@ -25,7 +25,7 @@ export default function About() {
         <h3>
           How to get started? Click here to Register for free if you’re a new
           user and follow the prompts. You will have the option to sign up for
-          one of our packages - or - if you’re still not sure if EasyRetro is
+          one of our packages - or - if you’re still not sure if OpenRetro is
           the right tool for your team, sign up for our 7-day free trial!
         </h3>
         <h3>Let the productive fun begin!</h3>
