192dba388d52e0458c19fa4464e635d322de74ac
Vishal Yadav <vishalyadaviit@gmail.com>
2024-02-26 19:59:24 +0530

    background color change for retros

--- src/pages/boards/boardId/retros/index.jsx
+++ src/pages/boards/boardId/retros/index.jsx
@@ -50,12 +50,12 @@ export default function Retros() {
 
   return (
     <div>
-      <div className="bg-[#F1F2F5] px-8 py-8">
+      <div className="bg-black px-8 py-8 text-white">
         <div className=" flex gap-8 flex-wrap items-center py-4">
           <NewRetroModal getBoardRetros={getBoardRetros}>
             <button
               type="button"
-              className="border-2 border-neutral-600 hover:border-blue-500 hover:text-blue-500 border-dashed h-40 w-60 flex flex-col gap-1 justify-center items-center rounded-md"
+              className="border-2 border-neutral-600 hover:border-gray-400 hover:text-gray-400 border-dashed h-40 w-60 flex flex-col gap-1 justify-center items-center rounded-md"
             >
               <div className=" bg-[#C0C0D4] rounded-full px-2 py-2">
                 <BaseIcon
