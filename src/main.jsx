e1490b19e1356e9652e4188bfd8d456ce555b1ef
Lakshya yadav <itslakshyayadav@gmail.com>
2024-03-08 20:13:22 +0530

    react-ga4 installed now

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 import router from "./router.jsx";
 import { RouterProvider } from "react-router-dom";
 import "./index.css";
-import ReactGA from "react-ga";
+import ReactGA from "react-ga4";
 
 const TRACKING_ID = "UA-281986007-1";
 ReactGA.initialize(TRACKING_ID);
