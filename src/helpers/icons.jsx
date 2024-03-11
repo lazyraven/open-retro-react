1692d488a0f852ba09092ceb16455e3839587489
Vishal Yadav <vishalyadaviit@gmail.com>
2024-03-11 23:34:47 +0530

    Home UI improved of OpenRetro

--- src/helpers/icons.jsx
+++ src/helpers/icons.jsx
@@ -16,6 +16,21 @@ const icons = {
       />
     </svg>
   ),
+  [ICONS.PlusCircle]: (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="0 0 24 24"
+      strokeWidth={1.5}
+      stroke="currentColor"
+    >
+      <path
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        d="M12 9v6m3-3H9m12 0a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"
+      />
+    </svg>
+  ),
   [ICONS.ellipsisvertical]: (
     <svg
       xmlns="http://www.w3.org/2000/svg"
