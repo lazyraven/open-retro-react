9c0179dc3f8b3cf40cc85d85b1983f1eadf36c52
Vishal Yadav <vishalyadaviit@gmail.com>
2024-03-12 23:12:44 +0530

    retro delete edit functionality added

--- src/firebase.js
+++ src/firebase.js
@@ -2,27 +2,26 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 import { getDatabase } from "firebase/database";
 
-// const firebaseConfig = {
-//   apiKey: "AIzaSyCpBxBTcyVw9p8iSeZ2lpWamYJ2l24OECY",
-//   authDomain: "open-retro-react.firebaseapp.com",
-//   projectId: "open-retro-react",
-//   storageBucket: "open-retro-react.appspot.com",
-//   messagingSenderId: "56358085480",
-//   appId: "1:56358085480:web:ae9dad8682b737aa9becdd",
-//   measurementId: "G-6CF6DRXKQ2",
-//   databaseURL: "https://open-retro-react-default-rtdb.firebaseio.com/",
-// };
+const {
+  VITE_FIRBASE_API_KEY,
+  VITE_FIRBASE_AUTH_DOMAIN,
+  VITE_FIRBASE_PROJECT_ID,
+  VITE_FIRBASE_STORAGE_BUCKET,
+  VITE_FIRBASE_MESSAGING_SENDER_ID,
+  VITE_FIRBASE_APP_ID,
+  VITE_FIRBASE_MEASUREMENT_ID,
+  VITE_FIRBASE_DATABASE_URL,
+} = import.meta.env;
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDvZaTTMXEk__YWK1ATkOhG2CIj6dEpZ5E",
-  authDomain: "openretro-prod-58918.firebaseapp.com",
-  projectId: "openretro-prod",
-  storageBucket: "openretro-prod.appspot.com",
-  messagingSenderId: "63228248426",
-  appId: "1:63228248426:web:2607693030a31f877b2513",
-  measurementId: "G-ES9G3MB3NN",
-  databaseURL:
-    "https://openretro-prod-default-rtdb.asia-southeast1.firebasedatabase.app/",
+  apiKey: VITE_FIRBASE_API_KEY,
+  authDomain: VITE_FIRBASE_AUTH_DOMAIN,
+  projectId: VITE_FIRBASE_PROJECT_ID,
+  storageBucket: VITE_FIRBASE_STORAGE_BUCKET,
+  messagingSenderId: VITE_FIRBASE_MESSAGING_SENDER_ID,
+  appId: VITE_FIRBASE_APP_ID,
+  measurementId: VITE_FIRBASE_MEASUREMENT_ID,
+  databaseURL: VITE_FIRBASE_DATABASE_URL,
 };
 
 const app = initializeApp(firebaseConfig);
