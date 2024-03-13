4617f9ce0c1199cfaff0d1b6f1314d9e950eb602
Lakshya yadav <itslakshyayadav@gmail.com>
2024-03-13 23:22:27 +0530

    email send work on share modal

--- src/components/ExportBoard.jsx
+++ src/components/ExportBoard.jsx
@@ -4,18 +4,18 @@ import { toast } from "react-toastify";
 
 export default function ExportBoard(props) {
   const { board } = props;
-  const [isOpen, setIsOpen] = useState(false);
+  // const [isOpen, setIsOpen] = useState(false);
   const [email, setEmail] = useState("");
   const [isValidEmail, setIsValidEmail] = useState(false);
   const [errorMessage, setErrorMessage] = useState("");
 
-  const openModal = () => {
-    setIsOpen(true);
-  };
+  // const openModal = () => {
+  //   setIsOpen(true);
+  // };
 
-  const closeModal = () => {
-    setIsOpen(false);
-  };
+  // const closeModal = () => {
+  //   setIsOpen(false);
+  // };
 
   const handleEmailChange = (event) => {
     const inputEmail = event.target.value;
@@ -56,88 +56,49 @@ export default function ExportBoard(props) {
         throw new Error("Failed to send email data");
       }
       setEmail("");
-      toast.success(` Mail sent Successfully !!`);
-      setIsOpen(false);
+      toast.success(`A mail has been sent successfully to ${email}`);
     } catch (error) {
       console.error("Error:", error.message);
     }
   };
   return (
     <div>
-      <BaseButton
-        radius="rounded-none"
-        theme="SECONDARY"
-        size="XL"
-        onClick={openModal}
-      >
-        Export Board
-      </BaseButton>
-      {isOpen && (
-        <div
-          className="relative z-10 "
-          aria-labelledby="modal-title"
-          role="dialog"
-          aria-modal="true"
-        >
-          <div className="fixed inset-0 bg-zinc-700 bg-opacity-70 backdrop-blur-sm transition-opacity"></div>
-          <div className="fixed inset-0 z-10 w-screen overflow-y-auto">
-            <div className="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
-              <div className="relative transform overflow-hidden rounded-lg bg-white text-left  transition-all sm:my-8 sm:w-full sm:max-w-lg">
-                <div className="flex flex-col gap-y-6 p-8 bg-zinc-900  text-white">
-                  <div className="flex justify-center ">
-                    <p className="text-center text-blue-200">
-                      If you fill the email then you export your board to your
-                      mail address for storing your board ID. it is optional
-                    </p>
-                  </div>
-                  <form
-                    onSubmit={handleSubmit}
-                    className="flex flex-col gap-y-4 px-8"
-                  >
-                    <div className="flex flex-col gap-y-1">
-                      <div className="flex gap-1">
-                        <label htmlFor="" className="text-zinc-300">
-                          Email
-                        </label>
-                      </div>
-                      <input
-                        type="email"
-                        // name="boardName"
-                        value={email}
-                        onChange={handleEmailChange}
-                        required
-                        placeholder="example@gmail.com"
-                        className="bg-zinc-900 border-zinc-700 border rounded-sm py-1.5 px-3"
-                      />
-                    </div>
-                    {errorMessage && (
-                      <p style={{ color: "red" }}>{errorMessage}</p>
-                    )}
-
-                    <div className="flex gap-4 items-center justify-end mt-4">
-                      <button
-                        type="button"
-                        onClick={closeModal}
-                        className="px-4 py-1 rounded-sm text-white"
-                      >
-                        Cancel
-                      </button>
-
-                      <button
-                        type="submit"
-                        disabled={!isValidEmail}
-                        className="px-4 py-1 rounded-sm text-zinc-900 bg-zinc-100 hover:bg-zinc-200"
-                      >
-                        send
-                      </button>
-                    </div>
-                  </form>
-                </div>
-              </div>
-            </div>
-          </div>
+      <div className="flex flex-col gap-y-6">
+        <div className="flex justify-center ">
+          <p className="text-center text-zinc-200">
+            If you fill the email then you export your board to your mail
+            address for storing your board ID. it is optional
+          </p>
         </div>
-      )}
+        <form onSubmit={handleSubmit} className="flex flex-col gap-y-4 px-8">
+          <div className="flex flex-col gap-y-1">
+            <div className="flex gap-1">
+              <label htmlFor="" className="text-zinc-300">
+                Email
+              </label>
+            </div>
+            <input
+              type="email"
+              value={email}
+              onChange={handleEmailChange}
+              required
+              placeholder="example@gmail.com"
+              className="bg-zinc-800 border-zinc-700 border rounded-sm py-1.5 px-3"
+            />
+          </div>
+          {errorMessage && <p style={{ color: "red" }}>{errorMessage}</p>}
+
+          <div className="flex justify-end mt-4">
+            <button
+              type="submit"
+              disabled={!isValidEmail}
+              className="px-4 py-1 rounded-sm text-zinc-900 bg-zinc-100 hover:bg-zinc-200"
+            >
+              Send
+            </button>
+          </div>
+        </form>
+      </div>
     </div>
   );
 }
