4b0e81088e319e6f1310bad8905739f3c7ebd18a
Lakshya yadav <itslakshyayadav@gmail.com>
2024-03-18 20:53:56 +0530

    Sidenavbar changes added

--- src/pages/root.jsx
+++ src/pages/root.jsx
@@ -1,7 +1,6 @@
 import { Outlet } from "react-router-dom";
 import "react-toastify/dist/ReactToastify.css";
 import { ToastContainer } from "react-toastify";
-
 function Root() {
   return (
     <div>
